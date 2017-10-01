3fde7d161811936cf7f2cb996e86322ac47d1412
Rob Caelers <rob.caelers@gmail.com>
2017-10-01 15:26:01 +0200

    Free bluetooth classic heap memory
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- components/os/BeaconScanner.cpp
+++ components/os/BeaconScanner.cpp
@@ -140,7 +140,8 @@ BeaconScanner::init()
 {
   esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
   esp_bt_controller_init(&bt_cfg);
-  esp_bt_controller_enable(ESP_BT_MODE_BTDM);
+  esp_bt_controller_enable(ESP_BT_MODE_BLE);
+  esp_bt_controller_mem_release(ESP_BT_MODE_BTDM);
   esp_bluedroid_init();
   esp_bluedroid_enable();
 
