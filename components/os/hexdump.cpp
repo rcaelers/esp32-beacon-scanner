62285085e651e4157aa1cdbd229a1a7d8ba4b4a6
Rob Caelers <rob.caelers@gmail.com>
2017-10-29 00:08:00 +0200

    Add hexdump function
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

---
+++ components/os/hexdump.cpp
@@ -0,0 +1,75 @@
+// Copyright (C) 2017 Rob Caelers <rob.caelers@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include "os/hexdump.hpp"
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+
+namespace os
+{
+  void
+  hexdump(const uint8_t *data, std::size_t size)
+  {
+    std::cout << std::hex << std::setfill('0');
+
+    int num_lines = (size + 15) / 16;
+    int index = 0;
+
+    for (int line = 0; line < num_lines; line++)
+      {
+        std::cout << std::setw(8) << index;
+
+        for (int column = 0; column < 16; column++)
+          {
+            if (column % 8 == 0)
+              {
+                std::cout << ' ';
+              }
+
+            if (index + column < size)
+              {
+                std::cout << ' ' << std::setw(2) << (unsigned)data[index + column];
+              }
+            else
+              {
+                std::cout << "   ";
+              }
+          }
+
+        std::cout << "  ";
+        for (int column = 0; column < std::min(std::size_t(16), size - index); column++)
+          {
+            if (data[index + column] < 32)
+              {
+                std::cout << '.';
+              }
+            else
+              {
+                std::cout << data[index + column];
+              }
+          }
+
+        std::cout << "\n";
+        index += 16;
+      }
+  }
+}
