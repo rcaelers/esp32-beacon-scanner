129918496dc312229cc5da02b899c507370c1298
Rob Caelers <rob.caelers@gmail.com>
2018-02-23 23:23:07 +0100

    Fix travis build
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- travis-build.sh
+++ travis-build.sh
@@ -3,19 +3,21 @@
 cd main/certs
 
 if [ -f CA.crt -o -f esp32.crt -o -f esp32.key ]; then
-   echo "Certificates already exist. Not overwriting"
-   exit 0;
+    echo "Certificates already exist. Not overwriting"
+else
+    certstrap init --common-name "CA" --passphrase "test"
+    certstrap request-cert --passphrase "test" -cn esp32
+    certstrap sign esp32 --passphrase "test" --CA "CA"
+
+    cp out/CA.crt .
+    cp out/esp32.crt .
+    cp out/esp32.key .
+    chmod u+w,a+r *.crt *.key
 fi
 
-certstrap init --common-name "CA" --passphrase "test"
-certstrap request-cert --passphrase "test" -cn esp32
-certstrap sign esp32 --passphrase "test" --CA "CA"
-
-cp out/CA.crt .
-cp out/esp32.crt .
-cp out/esp32.key .
-
 cd ../..
+
+cp main/user_config.h.example main/user_config.h
 
 make defconfig
 make all
