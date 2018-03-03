943bcd1714eeaf801890a0585589567cf6ee6676
Rob Caelers <rob.caelers@gmail.com>
2018-03-03 22:09:55 +0100

    Support disconnecting from signals
    Signed-off-by: Rob Caelers <rob.caelers@gmail.com>

--- components/loopp/include/loopp/core/Signal.hpp
+++ components/loopp/include/loopp/core/Signal.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2017 Rob Caelers <rob.caelers@gmail.com>
+// Copyright (C) 2017, 2018 Rob Caelers <rob.caelers@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -23,6 +23,7 @@
 
 #include <list>
 #include <functional>
+#include <memory>
 
 #include "loopp/core/Mutex.hpp"
 #include "loopp/core/ScopedLock.hpp"
@@ -34,39 +35,117 @@ namespace loopp
     template<class T>
     class Signal;
 
+    namespace detail
+    {
+      using disconnector_type = std::function<void()>;
+    }
+
+    class Connection
+    {
+    public:
+      Connection() = default;
+
+      explicit Connection(std::weak_ptr<detail::disconnector_type> disconnector)
+        : disconnector(disconnector)
+      {
+      }
+
+      void disconnect()
+      {
+        std::shared_ptr<detail::disconnector_type> d = disconnector.lock();
+        if (d != nullptr)
+          {
+            (*d)();
+          }
+      }
+
+      bool operator==(const Connection &other) const
+      {
+        std::shared_ptr<detail::disconnector_type> d = disconnector.lock();
+        std::shared_ptr<detail::disconnector_type> dother = other.disconnector.lock();
+        return d == dother;
+      }
+
+      bool operator!=(const Connection &other) const
+      {
+        return !(this->operator==(other));
+      }
+
+    private:
+      std::weak_ptr<detail::disconnector_type> disconnector;
+    };
+
+    class ScopedConnection
+    {
+    public:
+      ScopedConnection() = default;
+
+      ScopedConnection(const Connection &connection)
+        : connection(connection)
+      {
+      }
+
+      ~ScopedConnection()
+      {
+        connection.disconnect();
+      }
+
+      ScopedConnection &operator=(const Connection &connection)
+      {
+        if (this->connection != connection)
+          {
+            this->connection.disconnect();
+            this->connection = connection;
+          }
+        return *this;
+      }
+
+      void disconnect()
+      {
+        connection.disconnect();
+      }
+
+    private:
+      Connection connection;
+    };
+
     template<class... Args>
     class Signal<void(Args...)>
     {
-    public:
-      using callback_type = std::function<void(Args...)>;
-      using slot_list_type = std::list<callback_type>;
+      using function_type = std::function<void(Args...)>;
+      using self_type = Signal<void(Args...)>;
 
+      struct Slot
+      {
+      public:
+        Slot(function_type fn)
+          : fn(std::move(fn))
+        {
+        }
+
+        function_type fn;
+        std::shared_ptr<detail::disconnector_type> disconnector;
+      };
+
+      using slot_list_type = std::list<Slot>;
+
+    public:
       Signal() = default;
       ~Signal() = default;
 
       Signal(const Signal &) = delete;
       Signal &operator=(const Signal &) = delete;
 
-      Signal(Signal &&lhs)
-        : mutex(std::move(lhs.mutex))
-        , slots(std::move(lhs.slots))
-      {
-      }
+      Signal(Signal &&lhs) = default;
+      Signal &operator=(Signal &&lhs) = default;
 
-      Signal &operator=(Signal &&lhs)
-      {
-        if (this != &lhs)
-          {
-            mutex = std::move(lhs.mutex);
-            slots = std::move(lhs.slots);
-          }
-        return *this;
-      }
-
-      void connect(const callback_type callback)
+      Connection connect(const function_type function)
       {
         ScopedLock l(mutex);
-        slots.push_back(callback);
+
+        typename slot_list_type::iterator slot_it = slots.emplace(slots.end(), function);
+        slot_it->disconnector = std::make_shared<detail::disconnector_type>([this, slot_it]() { disconnect(slot_it); });
+        return Connection{ std::weak_ptr<detail::disconnector_type>(slot_it->disconnector) };
       }
 
       void operator()(const Args &... args)
@@ -74,8 +153,15 @@ namespace loopp
         ScopedLock l(mutex);
         for (auto &slot : slots)
           {
-            slot(args...);
+            slot.fn(args...);
           }
+      }
+
+    private:
+      void disconnect(typename slot_list_type::iterator it)
+      {
+        ScopedLock l(mutex);
+        slots.erase(it);
       }
 
     private:
